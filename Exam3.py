fdadb05a0885869d5a91ea96aacd055768ed4e09
Annisa Hana <78837175+hanavimagustine789@users.noreply.github.com>
2021-02-16 12:09:01 +0700

    Add files via upload

---
+++ Exam3.py
@@ -0,0 +1,57 @@
+# Exam_ No 3
+
+# the ravel(...) function will parse the string. expected output examples are as follows:
+
+# # Function Initialization :
+# def ravel(...):
+      
+# # Use the function
+
+print(ravel('Purwadhika'))
+print(ravel('Digital'))
+print(ravel('Coding'))
+print(ravel('School'))
+
+
+# Output:
+# PPuPurPurwPurwaPurwadPurwadhPurwadhiPurwadhikPurwadhika
+# DDiDigDigiDigitDigitaDigital
+# CCoCodCodiCodinCoding
+# SScSchSchoSchooSchool
+
+
+# Function Initialization :
+def ravel():
+    try:
+        kalimat = input("Masukan Kalimat:")
+        kalimat1 = kalimat.append()
+        for i in kalimat:
+            if not kalimat.isalpha:
+                print("Tidak Menerima Angka")
+        elif k in kalimat=True
+            print(k[1::] * i, end='')
+        else:
+            huruf = huruf * i 
+            P = kalimat * i + huruf
+            u = kalimat * i + huruf.lower()
+            r = kalimat * i + huruf.lower()
+            w = kalimat * i + huruf.lower()
+            a = kalimat * i + huruf.lower()
+            d = kalimat * i + huruf.lower()
+            i = kalimat * i + huruf.lower()
+            k = kalimat * i + huruf.lower()
+            a = kalimat * i + huruf.lower()
+            hasil = (("P","u", "r", "w", "a", "d", "i", "k", "a") * i, end=" ") 
+            return hasil
+            if 
+except:
+    return "tidak menerima angka"
+
+print(ravel())      
+
+
+            
+
+                
+
+
